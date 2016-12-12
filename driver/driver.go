091f5238ede74fda9c26103e460b19e0d4622d9f
Michal Kostrzewa <kostrzewa.michal@o2.pl>
2016-12-12 13:00:18 +0100

    Implement basic tests. Change project hierarchy.

--- driver/driver.go
+++ driver/driver.go
@@ -1,7 +1,7 @@
 // Implemented according to
 // https://github.com/docker/libnetwork/blob/master/docs/remote.md
 
-package main
+package driver
 
 import (
 	"encoding/json"
@@ -10,38 +10,23 @@ import (
 	"github.com/Microsoft/hcsshim"
 	"github.com/Sirupsen/logrus"
 	"github.com/docker/go-plugins-helpers/network"
-	"github.com/docker/go-plugins-helpers/sdk"
 )
 
 const (
-	// DRIVERNAME is name of the driver that is to be specified during docker network creation
-	DRIVERNAME = "Contrail"
+	// Drivername is name of the driver that is to be specified during docker network creation
+	Drivername = "Contrail"
 )
 
-type dummyNetworkDriver struct{}
+type ContrailDriver struct{}
 
-func main() {
-	d := &dummyNetworkDriver{}
-	h := network.NewHandler(d)
-
-	config := sdk.WindowsPipeConfig{
-		// This will set permissions for Everyone user allowing him to open, write, read the pipe
-		SecurityDescriptor: "S:(ML;;NW;;;LW)D:(A;;0x12019f;;;WD)",
-		InBufferSize:       4096,
-		OutBufferSize:      4096,
-	}
-
-	h.ServeWindows("//./pipe/"+DRIVERNAME, DRIVERNAME, &config)
-}
-
-func (d *dummyNetworkDriver) GetCapabilities() (*network.CapabilitiesResponse, error) {
+func (d *ContrailDriver) GetCapabilities() (*network.CapabilitiesResponse, error) {
 	logrus.Println("=== GetCapabilities")
 	r := &network.CapabilitiesResponse{}
 	r.Scope = network.LocalScope
 	return r, nil
 }
 
-func (d *dummyNetworkDriver) CreateNetwork(req *network.CreateNetworkRequest) error {
+func (d *ContrailDriver) CreateNetwork(req *network.CreateNetworkRequest) error {
 	logrus.Println("=== CreateNetwork")
 	logrus.Println("network.NetworkID =", req.NetworkID)
 	logrus.Println(req)
@@ -89,26 +74,26 @@ func (d *dummyNetworkDriver) CreateNetwork(req *network.CreateNetworkRequest) er
 	return nil
 }
 
-func (d *dummyNetworkDriver) AllocateNetwork(req *network.AllocateNetworkRequest) (*network.AllocateNetworkResponse, error) {
+func (d *ContrailDriver) AllocateNetwork(req *network.AllocateNetworkRequest) (*network.AllocateNetworkResponse, error) {
 	logrus.Println("=== AllocateNetwork")
 	logrus.Println(req)
 	r := &network.AllocateNetworkResponse{}
 	return r, nil
 }
 
-func (d *dummyNetworkDriver) DeleteNetwork(req *network.DeleteNetworkRequest) error {
+func (d *ContrailDriver) DeleteNetwork(req *network.DeleteNetworkRequest) error {
 	logrus.Println("=== DeleteNetwork")
 	logrus.Println(req)
 	return nil
 }
 
-func (d *dummyNetworkDriver) FreeNetwork(req *network.FreeNetworkRequest) error {
+func (d *ContrailDriver) FreeNetwork(req *network.FreeNetworkRequest) error {
 	logrus.Println("=== FreeNetwork")
 	logrus.Println(req)
 	return nil
 }
 
-func (d *dummyNetworkDriver) CreateEndpoint(req *network.CreateEndpointRequest) (*network.CreateEndpointResponse, error) {
+func (d *ContrailDriver) CreateEndpoint(req *network.CreateEndpointRequest) (*network.CreateEndpointResponse, error) {
 	logrus.Println("=== CreateEndpoint")
 	logrus.Println(req)
 	logrus.Println(req.Interface)
@@ -120,20 +105,20 @@ func (d *dummyNetworkDriver) CreateEndpoint(req *network.CreateEndpointRequest) 
 	return r, nil
 }
 
-func (d *dummyNetworkDriver) DeleteEndpoint(req *network.DeleteEndpointRequest) error {
+func (d *ContrailDriver) DeleteEndpoint(req *network.DeleteEndpointRequest) error {
 	logrus.Println("=== DeleteEndpoint")
 	logrus.Println(req)
 	return nil
 }
 
-func (d *dummyNetworkDriver) EndpointInfo(req *network.InfoRequest) (*network.InfoResponse, error) {
+func (d *ContrailDriver) EndpointInfo(req *network.InfoRequest) (*network.InfoResponse, error) {
 	logrus.Println("=== EndpointInfo")
 	logrus.Println(req)
 	r := &network.InfoResponse{}
 	return r, nil
 }
 
-func (d *dummyNetworkDriver) Join(req *network.JoinRequest) (*network.JoinResponse, error) {
+func (d *ContrailDriver) Join(req *network.JoinRequest) (*network.JoinResponse, error) {
 	logrus.Println("=== Join")
 	logrus.Println(req)
 	logrus.Println("options:")
@@ -145,31 +130,31 @@ func (d *dummyNetworkDriver) Join(req *network.JoinRequest) (*network.JoinRespon
 	return r, nil
 }
 
-func (d *dummyNetworkDriver) Leave(req *network.LeaveRequest) error {
+func (d *ContrailDriver) Leave(req *network.LeaveRequest) error {
 	logrus.Println("=== Leave")
 	logrus.Println(req)
 	return nil
 }
 
-func (d *dummyNetworkDriver) DiscoverNew(req *network.DiscoveryNotification) error {
+func (d *ContrailDriver) DiscoverNew(req *network.DiscoveryNotification) error {
 	logrus.Println("=== DiscoverNew")
 	logrus.Println(req)
 	return nil
 }
 
-func (d *dummyNetworkDriver) DiscoverDelete(req *network.DiscoveryNotification) error {
+func (d *ContrailDriver) DiscoverDelete(req *network.DiscoveryNotification) error {
 	logrus.Println("=== DiscoverDelete")
 	logrus.Println(req)
 	return nil
 }
 
-func (d *dummyNetworkDriver) ProgramExternalConnectivity(req *network.ProgramExternalConnectivityRequest) error {
+func (d *ContrailDriver) ProgramExternalConnectivity(req *network.ProgramExternalConnectivityRequest) error {
 	logrus.Println("=== ProgramExternalConnectivity")
 	logrus.Println(req)
 	return nil
 }
 
-func (d *dummyNetworkDriver) RevokeExternalConnectivity(req *network.RevokeExternalConnectivityRequest) error {
+func (d *ContrailDriver) RevokeExternalConnectivity(req *network.RevokeExternalConnectivityRequest) error {
 	logrus.Println("=== RevokeExternalConnectivity")
 	logrus.Println(req)
 	return nil
